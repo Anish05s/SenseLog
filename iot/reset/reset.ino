89b491e3a9c45eb0251fee01f5276b19c561f9c4
Anish Sarkar <117503429+Anish05s@users.noreply.github.com>
2026-03-28 20:49:36 +0530

    Update reset.ino

--- iot/reset/reset.ino
+++ iot/reset/reset.ino
@@ -1,1 +1,12 @@
+void (* resetFunc)(void)=0;
+void setup(){
+  // put your setup code here, to run once:
+  resetFunc();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
 
